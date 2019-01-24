2c5ecd5fcf3163b1248c54a4fe751cbc02d441c5
Johnny Wu <johnny.wuster@gmail.com>
2019-01-24 14:46:02 +0800

    remove sensitive info

--- market_maker/_settings_base.py
+++ market_maker/_settings_base.py
@@ -11,8 +11,8 @@ import logging
 BASE_URL = "wss://www.bitmex.com/realtime"
 
 # The BitMEX API requires permanent API keys. Go to https://testnet.bitmex.com/app/apiKeys to fill these out.
-API_KEY = "jKajXA3VAoJT8kjHS0dy1hP6"
-API_SECRET = "C3bUluV7ZF3RjPrXSBuwB7ciyg1PPPsQHhS1n6MdXH4DShY2"
+API_KEY = ""
+API_SECRET = ""
 
 
 ########################################################################################################################
